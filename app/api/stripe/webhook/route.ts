d24be1b639a633438d9736a7ef4ed50a4bebd3b3
KiNG <odon66674@gmail.com>
2025-12-17 17:51:40 +0100

    Uply V1.1

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -6,7 +6,7 @@ import Stripe from 'stripe'
 import prisma from '@/lib/db'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2025-11-17.clover'
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
